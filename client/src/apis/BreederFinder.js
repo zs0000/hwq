a860baec313409634a0a354bfb56aa6b564fa9ea
zs0000 <84543723+zs0000@users.noreply.github.com>
2021-05-24 12:53:22 -0700

    a
    a

--- client/src/apis/BreederFinder.js
+++ client/src/apis/BreederFinder.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 const baseURL = process.env.NODE_ENV === 'production'
  ? "/api/v1/breeders" 
- : "http://localhost:3006/api/v1/breeders";
+ : "http://localhost:3001/api/v1/breeders";
 
 
 export default axios.create({
